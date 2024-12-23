84ecceca1c1d60ae3cd7cfd9bf3855155bbcab99
VortexHub <prem782782@gmail.com>
2024-12-23 14:45:11 +0700

    Update VortexHub.v1.2.lua

--- VortexHub.v1.1.lua
+++ VortexHub.v1.1.lua
@@ -1,4 +1,3 @@
-
 -- Import Fluent Library
 local Fluent = loadstring(game:HttpGet("https://github.com/dawid-scripts/Fluent/releases/latest/download/main.lua"))()
 local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/dawid-scripts/Fluent/master/Addons/SaveManager.lua"))()
